f464ef02841df3985132c44bf81275ce694c0816
RaccoonButler <raccoonutau@gmail.com>
2023-07-27 12:44:40 -0500

    Finished the username and password inputs

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUpForm = () => {
-    return (
-        <div>
-            <h2>Sign Up</h2>
-        </div>
-    )
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Hello 👋");
+  }
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br></br>
+        <br></br>
+        <label>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
