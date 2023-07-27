e0e37a953456bbe72ed8c9b15644681d92ba5ebd
RaccoonButler <raccoonutau@gmail.com>
2023-07-27 13:40:34 -0500

    added API call logic

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,7 +1,38 @@
 import React, { usestate } from 'react';
 
-const Authenticate = () => {
-    return <h2>Authenticate !</h2>;
+const Authenticate = ({token}) => {
+    const [successMessage, setSuccessMessage] = usestate(null);
+    const [error, setError] = usestate(null);
+
+    async function handleClick() {
+        try {
+          const response = await fetch(
+            "https://fsa-jwt-practice.herokuapp.com/signup",
+             {
+               method: "GET",
+               headers: {
+                 "Content-Type": "application/json",
+                 Authorization: `Bearer ${token}`,
+               }   
+              }
+           );
+    
+          const data = await response.json();
+          setSuccessMessage(data.message);
+          setError(null);
+        } catch (error) {
+          setError(error.message);
+        }
+      }
+
+    return (
+        <div>
+            <h2>Authenticate !</h2>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={handleClick}>Authenticate Token!</button>
+        </div>
+    ) 
 };
 
 function App() {
