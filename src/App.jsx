7d4063ba4c3e0a09e6161284e302b15ec1642ed5
RaccoonButler <raccoonutau@gmail.com>
2023-07-27 14:53:47 -0500

    passet setToken as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import React, { useState } from 'react';
 import SignUpForm from './components/SignUpForm.jsx';
 import Authenticate from './components/Authenticate.jsx';
 import './App.css'
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <div>
-      <SignUpForm />
-      <Authenticate />
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
     </div>
   )
 }
 
-export default App
+export default App;
