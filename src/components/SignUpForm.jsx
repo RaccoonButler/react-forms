344ac7fcaad3cd8e3652d9cc3ce6c7c646964584
RaccoonButler <raccoonutau@gmail.com>
2023-07-27 15:42:20 -0500

    added password authentication and css for errors

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,45 +5,67 @@ const SignUpForm = ({ setToken }) => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const [validationErrors, setValidationErrors] = useState({
+    username: "",
+    password: "",
+  });
+
   async function handleSubmit(event) {
     event.preventDefault();
 
-    try {
-      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      });
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      setError(error.message);
+    const errors = {};
+    if (username.length < 8) {
+      errors.username = "Username must be at least 8 characters long.";
+    }
+    if (password.length < 6) {
+      errors.password = "Password must be at least 6 characters long.";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+    } else {
+      setValidationErrors({});
+
+      try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        setError(error.message);
+      }
     }
   }
-
 
   return (
     <div>
       <h2>Sign Up</h2>
+      {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
-          Username:{" "}
+          Username{" "}
           <input
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
+        {validationErrors.username && <p className="error">{validationErrors.username}</p>}
         <br />
         <br />
         <label>
           Password:{" "}
           <input
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        {validationErrors.password && <p className="error">{validationErrors.password}</p>}
         <button>Submit</button>
       </form>
     </div>
